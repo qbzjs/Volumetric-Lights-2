aa055764042dcfa5a8a227e27e67199386c01dbb
P3D\nlepers <nicolaslepers176@gmail.com>
2023-01-25 15:52:59 +0100

    [SCRIPT] - smooth camera reset

--- Assets/Scripts/Character/RotateCamera.cs
+++ Assets/Scripts/Character/RotateCamera.cs
@@ -16,9 +16,9 @@ public class RotateCamera : MonoBehaviour
         Instance = this;
     }
     #endregion
-    
+
     [Header("Cinemachine")
-     ,Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+     , Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
     public GameObject CinemachineCameraTarget;
 
     [Tooltip("How far in degrees can you move the camera up")]
@@ -33,20 +33,16 @@ public class RotateCamera : MonoBehaviour
     [Tooltip("For locking the camera position on all axis")]
     public bool LockCameraPosition = false;
 
+
     // cinemachine
     private float _cinemachineTargetYaw;
     private float _cinemachineTargetPitch;
 
-
-    // private GameplayInputs _input;
     private GameObject _mainCamera;
 
     private bool _isCurrentDeviceMouse = true;
 
-    private Vector3 _cameraStartRotation;
     private float _rotateMultiplierJoystickOldInputSystem = 250f;
-
-    private const float _threshold = 0.01f;
 
     private float _timerRotateCamera = 0;
     private float _timeToUnlock = 1.5f;
@@ -58,7 +54,6 @@ public class RotateCamera : MonoBehaviour
             _mainCamera = Camera.main.gameObject;
         }
         _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
-        _cameraStartRotation = _mainCamera.transform.rotation.eulerAngles;
     }
 
     private void Update()
@@ -91,19 +86,26 @@ public class RotateCamera : MonoBehaviour
             }
             else
             {
-                var multiplier = _rotateMultiplierJoystickOldInputSystem;
+                float multiplier = _rotateMultiplierJoystickOldInputSystem;
                 _cinemachineTargetYaw += (Input.GetAxis("Joystick X") * multiplier) * deltaTimeMultiplier;
                 _cinemachineTargetPitch += (Input.GetAxis("Joystick Y") * multiplier) * deltaTimeMultiplier;
             }
         }
         else
         {
-            var rotation = CinemachineCameraTarget.transform.localRotation;
+            Quaternion rotation = CinemachineCameraTarget.transform.localRotation;
 
-            CinemachineCameraTarget.transform.localRotation = Quaternion.Slerp(rotation, Quaternion.Euler(new Vector3(rotation.x, 0, rotation.z)), Time.deltaTime * 2);
-
+            CinemachineCameraTarget.transform.localRotation = Quaternion.Slerp(rotation, Quaternion.Euler(new Vector3(0, 0, rotation.z)), Time.deltaTime * 2);
             _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
-            _cinemachineTargetPitch = 0;
+
+            if (CinemachineCameraTarget.transform.rotation.eulerAngles.x > 180)
+            {
+                _cinemachineTargetPitch = CinemachineCameraTarget.transform.rotation.eulerAngles.x - 360;
+            }
+            else
+            {
+                _cinemachineTargetPitch = CinemachineCameraTarget.transform.rotation.eulerAngles.x;
+            }
         }
 
         _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
@@ -111,6 +113,8 @@ public class RotateCamera : MonoBehaviour
 
         CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
             _cinemachineTargetYaw, 0.0f);
+
+
     }
 
 
@@ -132,7 +136,7 @@ public class RotateCamera : MonoBehaviour
         {
             _timerRotateCamera += Time.deltaTime;
         }
-        else if(_timerRotateCamera >= _timeToUnlock)
+        else if (_timerRotateCamera >= _timeToUnlock)
         {
             LockCameraPosition = false;
             _timerRotateCamera = 0;
