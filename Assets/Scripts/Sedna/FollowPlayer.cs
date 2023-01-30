26e9df2735df8e15d640f5e14bf0bf7993d39fbd
P3D\nlepers <nicolaslepers176@gmail.com>
2023-01-30 15:24:50 +0100

    [SCRIPT] - Sedna follow player and rotate

---
+++ Assets/Scripts/Sedna/FollowPlayer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform _posTarget;
+    [SerializeField] Transform _rotationKayak;
+
+    private Rigidbody _rigidbody;
+    private Vector3 _velocity;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    void LateUpdate()
+    {
+        Position();
+        Rotation();
+    }
+    private void Position()
+    {
+        Vector3 current = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 target = new Vector3(_posTarget.transform.position.x, 0, _posTarget.transform.position.z);
+        Vector3 pos = Vector3.SmoothDamp(current, target, ref _velocity, 0.5f);
+
+        Vector3 velocity = new Vector3(_velocity.x, _rigidbody.velocity.y, _velocity.z);
+
+        _rigidbody.velocity = velocity;
+    }
+    private void Rotation()
+    {
+        Quaternion targetQuaternion = Quaternion.Euler(new Vector3(0, _rotationKayak.rotation.eulerAngles.y, 0));
+
+        Quaternion rota = Quaternion.Slerp(_rotationKayak.rotation, targetQuaternion, 0.5f);
+
+        _rigidbody.MoveRotation(rota);
+    }
+}
