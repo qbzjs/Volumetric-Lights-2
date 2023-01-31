3a6eaff4df6e10add6f3d94f2c971afa556831b2
MatteoBenaissa <matteobenaissa@gmail.com>
2023-01-31 16:50:08 +0100

    [ASSET] - Tool ScenTransition imported

---
+++ Assets/Tools/SceneTransition/TransitionTypeEnum.cs
@@ -0,0 +1,14 @@
+namespace SceneTransition
+{ 
+    using System;
+    
+    [Serializable]
+    public enum TransitionType
+    {
+        Fade = 0,
+        Size = 1,
+        Slide = 2
+    }
+}
+
+
