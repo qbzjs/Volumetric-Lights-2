9ab145b6ed9214679ed633a0a1d7368737153239
MatteoBenaissa <matteobenaissa@gmail.com>
2023-02-02 15:03:26 +0100

    [SCRIPT] - Death State creation

---
+++ Assets/Scripts/Character/State/CharacterDeathState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Character.State
+{
+    public class CharacterDeathState : CharacterStateBase
+    {
+        public CharacterDeathState(CharacterManager characterManagerRef) : base(characterManagerRef)
+        {
+            
+        }
+
+        public override void EnterState(CharacterManager character)
+        {
+            Debug.Log("death");
+        }
+
+        public override void UpdateState(CharacterManager character)
+        {
+        }
+
+        public override void FixedUpdate(CharacterManager character)
+        {
+        }
+
+        public override void SwitchState(CharacterManager character)
+        {
+        }
+    }
+}
