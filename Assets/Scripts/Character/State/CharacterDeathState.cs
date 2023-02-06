24eef2bc0edb28aabebef412398f9f9a88ce249a
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2023-02-06 21:40:50 +0100

    [SCRIPT] - test transition scen in death state

--- Assets/Scripts/Character/State/CharacterDeathState.cs
+++ Assets/Scripts/Character/State/CharacterDeathState.cs
@@ -1,4 +1,5 @@
 ﻿using Kayak;
+using SceneTransition;
 using UnityEngine;
 
 namespace Character.State
@@ -6,11 +7,21 @@ namespace Character.State
     public class CharacterDeathState : CharacterStateBase
     {
         private KayakController _kayakController;
-        
+        private InputManagement _inputManagement;
+        private TransitionType _transitionType;
+
         public CharacterDeathState(CharacterManager characterManagerRef, KayakController kayakController) : base(characterManagerRef)
         {
             _kayakController = kayakController;
         }
+        public CharacterDeathState(CharacterManager characterManagerRef, InputManagement inputManagement, KayakController kayakController) : base(characterManagerRef)
+        {
+            _inputManagement = inputManagement;
+            _kayakController = kayakController;
+        }
+
+
+
 
         public override void EnterState(CharacterManager character)
         {
@@ -19,7 +30,17 @@ namespace Character.State
 
         public override void UpdateState(CharacterManager character)
         {
+            Transform checkpoint = CheckpointManager.Instance.CurrentChekpoint.TargetRespawnTransform;
             MakeBoatRotationWithBalance(_kayakController.Mesh);
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                //TransitionManager.LaunchTransitionIn(_transitionType);
+                _kayakController.transform.position = checkpoint.position;
+                _kayakController.transform.rotation = checkpoint.rotation;
+
+                CharacterNavigationState characterUnbalancedState = new CharacterNavigationState(_kayakController, _inputManagement, CharacterManagerRef);
+                CharacterManagerRef.SwitchState(characterUnbalancedState);
+            }
         }
 
         public override void FixedUpdate(CharacterManager character)
