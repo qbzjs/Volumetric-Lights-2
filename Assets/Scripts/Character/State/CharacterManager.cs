b9b0850bb66c8676c20a9466275f21cfcc64c4a6
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2023-02-07 20:29:47 +0100

    [SCRIPT] - Add timer for death state in character manager

--- Assets/Scripts/Character/State/CharacterManager.cs
+++ Assets/Scripts/Character/State/CharacterManager.cs
@@ -12,21 +12,27 @@ namespace Character.State
         [Header("References"), SerializeField] private KayakController _kayakController;
         public CharacterStateBase CurrentStateBase;
         [SerializeField] private InputManagement _inputManagement;
+
+        [Header("Transition death")]
         public TransitionManager TransitionManager;
+        [Range(1, 5)] public float TimeToPlayFadeOut = 2.0f;
+        [Range(1, 5)] public float TimeFadeInEnded = 1.5f;
+
+        [Space(15)]
         public CameraController CamController;
         public Transform KayakMesh, CharacterMesh;
 
         [Header("Balance"), SerializeField, Range(0, 1)] private float balanceLerpTo0Value = 0.01f;
         [ReadOnly] public bool LerpBalanceTo0 = true;
         [ReadOnly] public float Balance = 0f;
-        [Range(0,40), Tooltip("The limit over which the player will go in unbalance state")] 
+        [Range(0, 40), Tooltip("The limit over which the player will go in unbalance state")]
         public float BalanceLimit = 10f;
         [Range(0, 40), Tooltip("The limit over which the player will die")]
         public float BalanceDeathLimit = 15f;
         [Range(0, 40), Tooltip("The angle the player has to reach when unbalanced to get back balanced")]
         public float RebalanceAngle = 8f;
 
-        
+
         private void Awake()
         {
             CharacterNavigationState navigationState =
@@ -41,7 +47,7 @@ namespace Character.State
         private void Update()
         {
             CurrentStateBase.UpdateState(this);
-            
+
             BalanceManagement();
         }
         private void FixedUpdate()
@@ -67,9 +73,9 @@ namespace Character.State
         private void OnGUI()
         {
             GUI.skin.label.fontSize = 50;
-            
+
             GUI.color = Color.white;
-            GUI.Label(new Rect(10, 10, 500, 100), "Balance : " + Math.Round(Balance,1));
+            GUI.Label(new Rect(10, 10, 500, 100), "Balance : " + Math.Round(Balance, 1));
         }
 
         #endregion
