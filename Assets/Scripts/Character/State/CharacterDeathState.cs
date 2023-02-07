d5c6f90334dd6b4186fba4077978d06f9b360b0f
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2023-02-07 20:28:08 +0100

    [SCRIPT] - death update

--- Assets/Scripts/Character/State/CharacterDeathState.cs
+++ Assets/Scripts/Character/State/CharacterDeathState.cs
@@ -7,8 +7,13 @@ namespace Character.State
     {
         private KayakController _kayakController;
         private InputManagement _inputs;
+        private bool _transitionIn = false;
+        private bool _respawned = false;
 
-        public CharacterDeathState(CharacterManager characterManagerRef, KayakController kayakController, InputManagement inputs, MonoBehaviour monoBehaviour) : 
+        private float _timerToPlayFadeOut = 0;
+        private float _timerFadeInEnded = 0;
+
+        public CharacterDeathState(CharacterManager characterManagerRef, KayakController kayakController, InputManagement inputs, MonoBehaviour monoBehaviour) :
             base(characterManagerRef, monoBehaviour)
         {
             _kayakController = kayakController;
@@ -18,23 +23,48 @@ namespace Character.State
         public override void EnterState(CharacterManager character)
         {
             Debug.Log("death");
-            CharacterManagerRef.TransitionManager.LaunchTransitionIn(SceneTransition.TransitionType.Fade);
         }
 
         public override void UpdateState(CharacterManager character)
         {
             Transform checkpoint = CheckpointManager.Instance.CurrentCheckpoint.TargetRespawnTransform;
-            MakeBoatRotationWithBalance(_kayakController.Mesh);
-            if (Input.GetKeyDown(KeyCode.K))
-            {
-                _kayakController.transform.position = checkpoint.position;
-                _kayakController.transform.rotation = checkpoint.rotation;
 
+            //Rotate kayak at 180 in z with balance
+            if (CharacterManagerRef.Balance > 0 && CharacterManagerRef.Balance < 60)
+                CharacterManagerRef.Balance += 0.5f;
+            else if (CharacterManagerRef.Balance < 0 && CharacterManagerRef.Balance > -60)
+                CharacterManagerRef.Balance -= 0.5f;
+
+            //Transition In
+            if (Mathf.Abs(CharacterManagerRef.Balance) >= 60 && _transitionIn == false)
+            {
+                _transitionIn = true;
+                CharacterManagerRef.TransitionManager.LaunchTransitionIn(SceneTransition.TransitionType.Fade);
+            }
+            //Timer transition In
+            if (_transitionIn == true)
+                _timerFadeInEnded += Time.deltaTime;
+
+            if (_timerFadeInEnded >= CharacterManagerRef.TimeFadeInEnded)
+            {
+                _timerToPlayFadeOut += Time.deltaTime;
+                if (_respawned == false)
+                {
+                    _respawned = true;
+                    //put kayak in checkpoint position & rotation
+                    _kayakController.transform.position = checkpoint.position;
+                    _kayakController.transform.rotation = checkpoint.rotation;
+                }
+
+                //Reset value
                 _kayakController.CanReduceDrag = true;
                 CharacterManagerRef.CamController.CanMoveCameraMaunally = true;
                 CharacterManagerRef.Balance = 0;
-                this.SwitchState(character);
+
+                if (_timerToPlayFadeOut >= CharacterManagerRef.TimeToPlayFadeOut)
+                    this.SwitchState(character);
             }
+            MakeBoatRotationWithBalance(_kayakController.Mesh);
         }
 
         public override void FixedUpdate(CharacterManager character)
@@ -43,7 +73,9 @@ namespace Character.State
 
         public override void SwitchState(CharacterManager character)
         {
+            //Transition out
             CharacterManagerRef.TransitionManager.LaunchTransitionOut(SceneTransition.TransitionType.Fade);
+            //Switch state
             CharacterNavigationState characterNavigationState = new CharacterNavigationState(_kayakController, _inputs, CharacterManagerRef, MonoBehaviourRef);
             CharacterManagerRef.SwitchState(characterNavigationState);
         }
