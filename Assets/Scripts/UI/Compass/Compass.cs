d99291e413fa05ed3eaec7fe521a1827ef9afb6d
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2023-03-12 17:36:04 +0100

    [SCRIPT] - add text angle value compass

--- Assets/Scripts/UI/Compass/Compass.cs
+++ Assets/Scripts/UI/Compass/Compass.cs
@@ -1,65 +1,53 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 public class Compass : MonoBehaviour
 {
     public RawImage CompassImage;
     public Transform Player;
     public TextMeshProUGUI CompassDirectionText;
 
+    #region test
+    Dictionary<int, string> displayAngle = new Dictionary<int, string>();
+
+    private void Start()
+    {
+        displayAngle.Add(0, "N");
+        displayAngle.Add(360, "N");
+        displayAngle.Add(45, "NE");
+        displayAngle.Add(90, "E");
+        displayAngle.Add(135, "SE");
+        displayAngle.Add(180, "S");
+        displayAngle.Add(225, "SW");
+        displayAngle.Add(270, "W");
+        displayAngle.Add(315, "NW");
+    }
+    #endregion
+
     public void Update()
     {
         CompassImage.uvRect = new Rect(Player.localEulerAngles.y / 360, 0, 1, 1);
 
         #region test
-        //Vector3 forward = Player.transform.forward;
+        Vector3 forward = Player.transform.forward;
 
-        //forward.y = 0;
+        forward.y = 0;
 
-        //float headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
-        //headingAngle = 5 * (Mathf.RoundToInt(headingAngle / 5.0f));
+        float headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
+        headingAngle = 5*Mathf.RoundToInt((headingAngle/5))+5;
 
-        //int displayangle;
-        //displayangle = Mathf.RoundToInt(headingAngle);
+        if (headingAngle > 360)
+        {
+            headingAngle -= 360;
+        }
 
-        //switch (displayangle)
-        //{
-        //    case 0:
-        //        //Do this
-        //        CompassDirectionText.text = "N";
-        //        break;
-        //    case 360:
-        //        //Do this
-        //        CompassDirectionText.text = "N";
-        //        break;
-        //    case 45:
-        //        //Do this
-        //        CompassDirectionText.text = "NE";
-        //        break;
-        //    case 90:
-        //        //Do this
-        //        CompassDirectionText.text = "E";
-        //        break;
-        //    case 130:
-        //        //Do this
-        //        CompassDirectionText.text = "SE";
-        //        break;
-        //    case 180:
-        //        //Do this
-        //        CompassDirectionText.text = "S";
-        //        break;
-        //    case 225:
-        //        //Do this
-        //        CompassDirectionText.text = "SW";
-        //        break;
-        //    case 270:
-        //        //Do this
-        //        CompassDirectionText.text = "W";
-        //        break;
-        //    default:
-        //        CompassDirectionText.text = headingAngle.ToString();
-        //        break;
-        //}
+
+        if (displayAngle.ContainsKey((int)headingAngle))
+            CompassDirectionText.text = displayAngle[(int)headingAngle];
+        else
+            CompassDirectionText.text = headingAngle.ToString();
+           
         #endregion
 
     }
