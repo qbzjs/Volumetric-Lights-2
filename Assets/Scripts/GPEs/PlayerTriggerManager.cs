77eafe9f22c94e1d2d1ba980cc2db6c59d9bea23
MatteoBenaissa <matteobenaissa@gmail.com>
2023-03-14 22:19:55 +0100

    [SCRIPT] - added an option to turn off the playertrigger box gizmo

--- Assets/Scripts/GPEs/PlayerTriggerManager.cs
+++ Assets/Scripts/GPEs/PlayerTriggerManager.cs
@@ -6,14 +6,15 @@ namespace GPEs
 {
     public class PlayerTriggerManager : MonoBehaviour
     {
-        [Header("TriggerBox"), SerializeField] private Vector3 _triggerSize = Vector3.one;
+        [Header("TriggerBox"), SerializeField] private bool _showTriggerGizmos = true;
+        [SerializeField] private Vector3 _triggerSize = Vector3.one;
         [SerializeField] private Vector3 _triggerOffsetPosition = Vector3.zero;
         [SerializeField] private LayerMask _playerLayerMask;
         [Header("Event")] public UnityEvent OnPlayerDetected = new UnityEvent();
 
         protected KayakController KayakController;
 
-        protected virtual void Update()
+        public virtual void Update()
         {
             RaycastHit[] hits = Physics.BoxCastAll(transform.position + _triggerOffsetPosition, _triggerSize / 2, Vector3.forward, Quaternion.identity, 0f, _playerLayerMask);
             foreach (RaycastHit hit in hits)
@@ -31,8 +32,11 @@ namespace GPEs
 
         public virtual void OnDrawGizmos()
         {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireCube(transform.position + _triggerOffsetPosition, _triggerSize);
+            if (_showTriggerGizmos)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireCube(transform.position + _triggerOffsetPosition, _triggerSize);
+            }
         }
 
 #endif
